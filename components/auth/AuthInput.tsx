a355f4a2091f2b85f115b18fe72e05002a71ee54
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2026-02-03 16:36:35 +0000

    feat: implement Redux products slice with async thunks

--- components/auth/AuthInput.tsx
+++ components/auth/AuthInput.tsx
@@ -11,4 +11,4 @@ const AuthInput = ({ label, ...props }: Props) => {
             className="border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-indigo-600" />
         </div>
     );
-}
+};
