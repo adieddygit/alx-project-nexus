47e3d1b32da6cf96e627d9327873edf992377968
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2026-02-17 19:25:25 +0000

    feat: fixed hydration errors

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,10 +7,10 @@ export default function AboutPage() {
         <main className="max-w-6xl mx-auto px-6 py-20 space-y-16">
             {/* Hero / Intro */}
             <section className="text-center">
-                <h1 className="text-4xl md:text-5xl font-bold mb-4">
+                <h1 className="text-4xl md:text-5xl mb-4">
                  Ne<span className="text-5xl">╳</span>us<span className="text-indigo-600"></span><span className="text-indigo-500">Store</span>
                 </h1>
-                <p className="font-semibold text-lg md:text-xl text-indigo-300 max-w-2xl mx-auto">
+                <p className="text-lg md:text-xl text-indigo-300 max-w-2xl mx-auto">
                 NexusStore is a modern e-commerce PWA built for a seamless shopping experience.
                 Our goal is to provide a fast, responsive, and user-friendly platform for everyone.
                 </p>
@@ -19,8 +19,8 @@ export default function AboutPage() {
             {/* Mision / vision */}
             <section className="grid md:grid-cols-2 gap-10 items-center">
                 <div>
-                    <h2 className="text-xl font-bold mb-4">Our Mission</h2>
-                    <p className="font-semibold text-indigo-300">
+                    <h2 className="text-2xl mb-4">Our Mission</h2>
+                    <p className=" text-indigo-300">
                     We aim to bring the best products to your fingertips while maintaining
                     high performance, accessibility, and a beautiful design.
                     </p>
@@ -38,7 +38,7 @@ export default function AboutPage() {
 
             {/* Team / Values */}
             <section>
-                <h2 className="text-3xl font-bold text-center mb-8">Our Values</h2>
+                <h2 className="text-3xl text-center mb-8">Our Values</h2>
         <div className="grid md:grid-cols-3 gap-8 text-center">
           <div className="p-6 bg-white rounded-xl shadow hover:shadow-lg transition">
             <h3 className="text-black font-semibold text-xl mb-2">Quality</h3>
