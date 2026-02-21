be55a6d61d861d852d44cc2186e9ef1e5a583f90
adiemmanuel10@gmail.com <adiemmanuel10@gmail.com>
2026-02-21 05:22:03 +0000

    feat:updated the product pages

--- app/product/[id]/not-found.tsx
+++ app/product/[id]/not-found.tsx
@@ -1,8 +1,8 @@
 export default function NotFound() {
     return (
         <div className="min-h-[60vh] flex flex-2 text-center items-center justify-center ">
-            <h2 className="text-2xl font-bold">Product not found</h2>
-            <p className="mt-2 text-gray-300">The product you&apos;re looking for is not found</p>
+            {/* <h2 className="text-2xl font-bold">Product not found</h2> */}
+            <p className="mt-2 text-gray-400">The product you&apos;re looking for is not found</p>
         </div>
     )
 }
